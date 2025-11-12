83f341c4c4ad7258572a3fd59c49458fce174832
BhashiBandara95 <ugabkanishka@gmail.com>
2025-11-13 01:43:46 +0530

    modified:   App.jsx 	modified:   ExploreMenu.css 	modified:   Footer.css 	modified:   Header.css 	create new file:   LoginPopup.css 	create new file:   LoginPopup.jsx 	modified:   Navbar.css 	modified:   Navbar.jsx 	modified:   index.css

--- fronend/src/components/Navbar/Navbar.jsx
+++ fronend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 	// useState function
 	const [menu, setMenu] = useState("menu");
 	return (
@@ -10,30 +11,34 @@ const Navbar = () => {
 			<img src={assets.logo} alt="" className="logo" />
 			<ul className="navbar-menu">
 				{/* use dynamic class names */}
-				<li
+				<Link
+					to="/"
 					onClick={() => setMenu("home")}
 					className={menu === "home" ? "active" : ""}
 				>
 					home
-				</li>
-				<li
+				</Link>
+				<a
+					href="#explore-menu"
 					onClick={() => setMenu("menu")}
 					className={menu === "menu" ? "active" : ""}
 				>
 					menu
-				</li>
-				<li
+				</a>
+				<a
+					href="#app-download"
 					onClick={() => setMenu("mobile-app")}
 					className={menu === "mobile-app" ? "active" : ""}
 				>
 					mobile-app
-				</li>
-				<li
+				</a>
+				<a
+					href="#footer"
 					onClick={() => setMenu("contact-us")}
 					className={menu === "contact-us" ? "active" : ""}
 				>
 					contact us
-				</li>
+				</a>
 			</ul>
 			<div className="navbar-right">
 				<img src={assets.search_icon} alt="" />
@@ -41,7 +46,7 @@ const Navbar = () => {
 					<img src={assets.basket_icon} alt="" />
 					<div className="dot"></div>
 				</div>
-				<button>Sign In</button>
+				<button onClick={() => setShowLogin(true)}>Sign In</button>
 			</div>
 		</div>
 	);
